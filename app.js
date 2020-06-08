9498312a5ff3c7b62ee1d5a5771a35049ff855f5
Martin Harrison <martin.harrison@nology.io>
2020-06-08 15:48:42 +0100

    create base html and css

---
+++ app.js
@@ -0,0 +1,25 @@
+// Get HTML elements
+
+
+// Start/reset game
+
+// Import dictionary and create random words
+
+// Animate words coming across and down
+
+// Capture player types
+
+// Handle keyboard input for mobile
+
+// Delete words when player has typed
+
+// Game over when word reaches bottom
+
+// Non-MVP:
+// Show player progress in word
+// Words firing letters at player
+// Alter difficulty (speed/word length) 
+// Keep tally of words typed & wpm 
+
+// - MUST make use of functions to neatly organise code.
+// - MUST be hosted on your Github with at least 15 git commits.
