e6aa11d3c33fb54788f49684b3007daf82f4dfa7
Martin Harrison <martin.harrison@nology.io>
2020-06-08 20:10:59 +0100

    generate word array from given string

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 // Get HTML elements
+const gameContainer = document.querySelector('#game-container');
+const startButton = document.querySelector('button');
 
+// Create variables 
+let gameOver = 0;
+let wordsTyped = 0;
+let wordArray = [];
 
 // Start/reset game
+startButton.addEventListener('click', () => {
+    gameOver = 0;
+    wordsTyped = 0;
+    wordArray = 0;
+})
 
 // Import dictionary and create random words
 
-// Animate words coming across and down
+// Fetch 100 words from API
+// const callAPI = () => {
+//     const Http = new XMLHttpRequest();
+//     const url='https://random-word-api.herokuapp.com/word?number=100';
+//     Http.open("GET", url);
+//     Http.send();
+
+//     Http.onreadystatechange = () => {
+//     console.log(Http.responseText)
+//     }
+// }
+// callAPI();
+
+// let generatedWord = () => {
+//     ParseInt(Math.random() * 100);
+// }
+
+// Generate random 'words'
+
+// const letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// let generatedWord = (array) => {
+//     return array(ParseInt(Math.random() * 26));
+// }
+// console.log(generatedWord(letterArray));
+
+// Generate word array from string
+let string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error saepe libero quaerat harum quas dignissimos corrupti, hic esse mollitia ipsam explicabo vitae obcaecati quisquam eum, et repellendus! Odio, facilis atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facere quidem cupiditate expedita, neque maiores blanditiis ex porro assumenda vitae amet rerum sequi, quos ab dignissimos illum, aspernatur ducimus. Earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptates iusto quas eius, nesciunt eos labore. Placeat consequatur nihil tempore magnam, id repellat asperiores, eveniet rem architecto itaque suscipit veniam";
+
+wordArray = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(' ');
+console.log(wordArray);
+
+// Animate words coming across and down - push words onto new array
 
 // Capture player types
+
+// // Keyboard event info: https://javascript.info/keyboard-events
+// document.addEventListener('keydown', (event) => {
+//     // if (event.key == 'z') {
+//       console.log(event.key);
+//     // }
+//   });
 
 // Handle keyboard input for mobile
 
